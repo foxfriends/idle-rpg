b7ec946784ff902a900adfab43f47428e601187e
Cameron Eldridge <cameron_e@rogers.com>
2016-09-23 00:36:51 -0400

    Add fixed text-width elements
    - For displaying text that might change width, and for easier padding
      of text to be a specific width

---
+++ public_html/script/hud/index.js
@@ -0,0 +1,4 @@
+// Manage the HUD, showing the number of balls, menus, and other things.
+'use strict';
+const balls = 10; // TODO: implement the ball system, and expose the ball count
+document.querySelector('#balls').setAttribute('data-content', `Balls: ${balls}`);
--- public_html/script/index.js
+++ public_html/script/index.js
@@ -1,2 +1,7 @@
 'use strict';
 import '../style/index.scss';
+import './hud';
+
+// TODO: re-call this every time the scene changes, instead of just here
+import watchPadding from './watch-padding';
+watchPadding();
