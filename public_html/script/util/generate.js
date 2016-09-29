5399df6bdcb2f31770c5f50f796042bce68d4a5c
Cameron Eldridge <cameron_e@rogers.com>
2016-09-29 10:44:01 -0400

    Reorganize util file for consistency

--- public_html/script/util/generate.js
+++ public_html/script/util/generate.js
@@ -1,11 +1,9 @@
-/* (generator: Generator([...T]) => S, thisArg: any, ...args: [, ...T]) => Promise<S>
-
-  Automatically progresses Promise based generators as their values become
-  available.
-
-  Basically a wrapper around generators to make them work like async functions.
-*/
+// (generator: Generator([...T]) => S, thisArg: any, ...args: [, ...T]) => Promise<S>
+//    Automatically progresses Promise based generators as their values become
+//    available.
+//    Basically a wrapper around generators to make them work like async functions.
 'use strict';
+
 export default (generator, that, ...args) => new Promise((resolve, reject) => {
     const run = generator.call(that, ...args);
     // run the next step of the generator
--- public_html/script/util/pad.js
+++ public_html/script/util/pad.js
@@ -1,12 +1,11 @@
-// Provide utility functions that are likely to be needed in many places
-'use strict';
 // pad(str: string, len: number, padding: string = ' '): string
 //    pads the given string str to ensure that its length is len. The string
 //    used for padding can be specified.
-function pad(str, len, padding = ' ') {
+'use strict';
+
+export default (str, len, padding = ' ') => {
   while(str.length < len) {
     str += padding;
   }
   return str;
-}
-export { pad };
+};
